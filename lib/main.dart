1838ebc0ec7e4d69d13c906f11aecec15192f76c
farahamr76 <farahamr776@gmail.com>
2024-07-06 18:33:00 +0300

    first Commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Daleel());
+}
+
+class Daleel extends StatelessWidget {
+  const Daleel({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
